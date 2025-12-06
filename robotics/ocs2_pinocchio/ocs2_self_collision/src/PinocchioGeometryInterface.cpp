8f4ddaa98e2af4f9a85df40868b8a3f4368dbc7e
wei-hsuan-cheng <johnathancheng0125@gmail.com>
2025-12-06 13:01:32 +0800

    update include dependencies

--- robotics/ocs2_pinocchio/ocs2_self_collision/src/PinocchioGeometryInterface.cpp
+++ robotics/ocs2_pinocchio/ocs2_self_collision/src/PinocchioGeometryInterface.cpp
@@ -37,12 +37,6 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <pinocchio/multibody/model.hpp>
 #include <pinocchio/parsers/urdf.hpp>
 
-#if defined(__has_include)
-#    if __has_include(<pinocchio/collision/distance.hpp>)
-#        include <pinocchio/collision/distance.hpp>
-#    endif
-#endif
-
 #ifdef URDFDOM_VERSION_GT_4
 #include <tinyxml2.h>
 #endif
