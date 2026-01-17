d132f75a602a6a1fef3c77d9650e9eed8f9fabb6
wei-hsuan-cheng <johnathancheng0125@gmail.com>
2026-01-17 14:47:33 +0800

    Rearrange dependencies ordf (no change in code content)

--- basic examples/ocs2_mobile_manipulator_ros/src/MobileManipulatorSqpMpcNode.cpp
+++ basic examples/ocs2_mobile_manipulator_ros/src/MobileManipulatorSqpMpcNode.cpp
@@ -27,10 +27,10 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  ******************************************************************************/
 
+#include <ocs2_sqp/SqpMpc.h>
 #include <ocs2_mobile_manipulator/MobileManipulatorInterface.h>
 #include <ocs2_ros_interfaces/mpc/MPC_ROS_Interface.h>
 #include <ocs2_ros_interfaces/synchronized_module/RosReferenceManager.h>
-#include <ocs2_sqp/SqpMpc.h>
 
 #include "rclcpp/rclcpp.hpp"
 
